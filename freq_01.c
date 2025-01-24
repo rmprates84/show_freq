71cfdc3d1fbdea60db45db0b674f1f57cb6754cc
Ricardo <ricardo.prates@univasf.edu.br>
2025-01-23 21:00:27 -0300

    primeiro commit

---
+++ freq_01.c
@@ -0,0 +1,43 @@
+#include <stdio.h> //Biblioteca padrão do C
+#include "pico/stdlib.h"     // Biblioteca padrão para funções básicas de E/S, temporização, controle de GPIO, etc.
+#include "hardware/clocks.h" // Biblioteca para configuração e gerenciamento dos clocks do microcontrolador RP2040.
+#include "hardware/pll.h"    // Biblioteca para controle e configuração dos Phase-Locked Loops (PLLs).
+
+//função para obter as 
+void measure_freqs(void) {
+    uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+    uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+    uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+    uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+    uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+    uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+    uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+    uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+
+    printf("pll_sys  = %dkHz\n", f_pll_sys);
+    printf("pll_usb  = %dkHz\n", f_pll_usb);
+    printf("rosc     = %dkHz\n", f_rosc);
+    printf("clk_sys  = %dkHz\n", f_clk_sys);
+    printf("clk_peri = %dkHz\n", f_clk_peri);
+    printf("clk_usb  = %dkHz\n", f_clk_usb);
+    printf("clk_adc  = %dkHz\n", f_clk_adc);
+    printf("clk_rtc  = %dkHz\n", f_clk_rtc);
+}
+
+int main() //função principal
+{
+
+// Inicializa a comunicação serial, permitindo o uso das funções padrão de E/S como printf.
+stdio_init_all();
+
+//coloca a frequência de clock para 128 MHz
+set_sys_clock_khz(128000, false);
+
+while (true)
+{
+    printf("imprimindo frequências\r\n");
+    measure_freqs(); //imprimir frequências
+    sleep_ms(5000); //delay
+}
+
+}
